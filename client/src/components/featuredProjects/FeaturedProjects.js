3d6b1d84468eafabfad4e06686a2341fe26e4120
Andy Lindsay <andydlindsay@gmail.com>
2017-10-10 22:27:03 -0700

    fleshes out FeaturedProjects component

--- client/src/components/featuredProjects/FeaturedProjects.js
+++ client/src/components/featuredProjects/FeaturedProjects.js
@@ -1,12 +1,68 @@
 import React, { Component } from 'react';
 import './FeaturedProjects.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import myTheme from '../../styles/myTheme';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui-next/Grid';
 
 class FeaturedProjects extends Component {
+    style = {
+        height: 'auto',
+        textAlign: 'center'
+    }
+
     render() {
         return (
-            <div className="FeaturedProjects">
-                Featured Projects
-            </div>
+            <MuiThemeProvider muiTheme={getMuiTheme(myTheme)}>
+                <div className="FeaturedProjects">
+                    <Paper 
+                        style={this.style}
+                        zDepth={0}
+                    >
+                        <Grid
+                            container
+                            spacing={0}
+                            align={'center'}
+                            justify={'center'}
+                        >
+                            <Grid item xs={12}>
+                                <span className="headline2">PROJECTS</span>
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <Paper zDepth={0} className="project">
+                                    <img src="https://s3.amazonaws.com/andydlindsay-portfolio/recipe+box/recipe+box+home+cropped.png" alt="recipe box app" />
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <Paper zDepth={0} className="project">
+                                    <img src="https://s3.amazonaws.com/andydlindsay-portfolio/game+of+life/game+of+life+cropped.png" alt="game of life app" />
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <Paper zDepth={0} className="project">
+                                    <img src="https://s3.amazonaws.com/andydlindsay-portfolio/calculator/calculator.png" alt="calculator app" />
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <Paper zDepth={0} className="project">
+                                    <img src="https://s3.amazonaws.com/andydlindsay-portfolio/tic+tac+toe/tic+tac+toe+cropped.png" alt="tic tac toe app" />
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <Paper zDepth={0} className="project">
+                                    <img src="https://s3.amazonaws.com/andydlindsay-portfolio/pomodoro/pomodoro+cropped.png" alt="pomodoro timer app" />
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <Paper zDepth={0} className="project">
+                                    <img src="https://s3.amazonaws.com/andydlindsay-portfolio/simon/simon+cropped.png" alt="simon game" />
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -4,26 +4,57 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import myTheme from '../../styles/myTheme';
 import BottomNavigation, { BottomNavigationButton } from 'material-ui-next/BottomNavigation';
+import Grid from 'material-ui-next/Grid';
+import Paper from 'material-ui/Paper';
 
 class Footer extends Component {
+    paperStyles = {
+        height: 'auto',
+        textAlign: 'center'
+    }
+
     render() {
         return (
             <MuiThemeProvider muiTheme={getMuiTheme(myTheme)}>
                 <div className="Footer">
-                    <BottomNavigation value="" showLabels>
-                        <BottomNavigationButton
-                            label="Codepen"
-                            icon={<i className="fa fa-codepen fa-2x"></i>}
-                        />
-                        <BottomNavigationButton
-                            label="LinkedIn"
-                            icon={<i className="fa fa-linkedin fa-2x"></i>}
-                        />
-                        <BottomNavigationButton
-                            label="Github"
-                            icon={<i className="fa fa-github fa-2x"></i>}
-                        />
-                    </BottomNavigation>
+                    <Grid
+                        container
+                        spacing={0}
+                        align={'center'}
+                        justify={'center'}
+                    >
+                        <Grid item xs={12} lg={6}>
+                            <Paper
+                                style={this.paperStyles}
+                                zDepth={0}
+                            >
+                                <span className="footer-text">Andy is based in Vancouver, Canada</span>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} lg={6}>
+                            <Paper
+                                style={this.paperStyles}
+                                zDepth={0}
+                            >
+                                <div className="bottom-navigation">
+                                    <BottomNavigation value="" showLabels>
+                                        <BottomNavigationButton
+                                            label="Codepen"
+                                            icon={<i className="fa fa-codepen fa-2x"></i>}
+                                        />
+                                        <BottomNavigationButton
+                                            label="LinkedIn"
+                                            icon={<i className="fa fa-linkedin fa-2x"></i>}
+                                        />
+                                        <BottomNavigationButton
+                                            label="Github"
+                                            icon={<i className="fa fa-github fa-2x"></i>}
+                                        />
+                                    </BottomNavigation>
+                                </div>
+                            </Paper>
+                        </Grid>
+                    </Grid>
                 </div>
             </MuiThemeProvider>
         );
