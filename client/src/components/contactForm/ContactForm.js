a5515f791b67f0bb82d9ed5612ca4e05bc69f738
Andy Lindsay <andydlindsay@gmail.com>
2017-10-12 15:41:34 -0700

    adds redux-form to ContactForm component

--- client/src/components/contactForm/ContactForm.js
+++ client/src/components/contactForm/ContactForm.js
@@ -7,32 +7,67 @@ import Paper from 'material-ui/Paper';
 import Grid from 'material-ui-next/Grid';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import { connect } from 'react-redux';
+import { createMessage } from '../../actions';
+import { Field, reduxForm } from 'redux-form';
 
 class ContactForm extends Component {
     styles = {
         paperStyle: {
             height: 'auto',
             textAlign: 'center'
-        },
-        underlineStyle: {
-            borderColor: myTheme.palette.textColor
-        },
-        underlineFocusStyle: {
-            borderColor: myTheme.palette.textColor
-        },
-        floatingLabelStyle: {
-            color: myTheme.palette.textColor,
-            fontSize: '22px'
-        },
-        floatingLabelFocusStyle: {
-            color: myTheme.palette.textColor
-        },
-        textFieldStyle: {
-            textAlign: 'left'
         }
     }
 
+    onSubmit(values) {
+        this.props.createMessage(values, () => {
+            this.props.reset();
+        });
+    }
+
+    renderField(field) {
+        const styles = {
+            underlineStyle: {
+                borderColor: myTheme.palette.textColor
+            },
+            underlineFocusStyle: {
+                borderColor: myTheme.palette.textColor
+            },
+            floatingLabelStyle: {
+                color: myTheme.palette.textColor,
+                fontSize: '22px'
+            },
+            floatingLabelFocusStyle: {
+                color: myTheme.palette.textColor
+            },
+            textFieldStyle: {
+                textAlign: 'left'
+            }
+        }
+        const { meta: { touched, error } } = field;
+        const errorText = `${touched && error ? error : ''}`;
+        const numRows = field.multiLine ? field.rows : 1;
+        const multiLine = field.multiLine ? true : false;
+        return (
+            <TextField
+                floatingLabelFixed={true}
+                floatingLabelText={field.label}
+                fullWidth={true}
+                style={styles.textFieldStyle}
+                underlineStyle={styles.underlineStyle}
+                underlineFocusStyle={styles.underlineFocusStyle}
+                floatingLabelStyle={styles.floatingLabelStyle}
+                floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+                errorText={errorText}
+                multiLine={multiLine}
+                rows={numRows}
+                {...field.input}
+            />
+        );
+    }
+
     render() {
+        const { handleSubmit } = this.props;
         return (
             <MuiThemeProvider muiTheme={getMuiTheme(myTheme)}>
                 <div className="ContactForm">
@@ -51,39 +86,42 @@ class ContactForm extends Component {
                                 <span className="headline2">CONTACT ME</span>
                             </Grid>
                             <Grid item xs={12}>
-                                <form className="contact-form">
-                                    <TextField
-                                        floatingLabelFixed={true}
-                                        floatingLabelText="Name"
-                                        fullWidth={true}
-                                        underlineStyle={this.styles.underlineStyle}
-                                        underlineFocusStyle={this.styles.underlineFocusStyle}
-                                        floatingLabelStyle={this.styles.floatingLabelStyle}
-                                        floatingLabelFocusStyle={this.styles.floatingLabelFocusStyle}
-                                    /><br />
-                                    <TextField
-                                        floatingLabelFixed={true}
-                                        floatingLabelText="Email"
-                                        fullWidth={true}
-                                        underlineStyle={this.styles.underlineStyle}
-                                        underlineFocusStyle={this.styles.underlineFocusStyle}
-                                        floatingLabelStyle={this.styles.floatingLabelStyle}
-                                        floatingLabelFocusStyle={this.styles.floatingLabelFocusStyle}
-                                    /><br />
-                                    <TextField
-                                        floatingLabelFixed={true}
-                                        floatingLabelText="Message"
-                                        fullWidth={true}
-                                        multiLine={true}
-                                        rows={3}
-                                        style={this.styles.textFieldStyle}
-                                        underlineStyle={this.styles.underlineStyle}
-                                        underlineFocusStyle={this.styles.underlineFocusStyle}
-                                        floatingLabelStyle={this.styles.floatingLabelStyle}
-                                        floatingLabelFocusStyle={this.styles.floatingLabelFocusStyle}
-                                    /><br />
+                                <form 
+                                    className="contact-form"
+                                    name="MessagesNewForm"
+                                    onSubmit={
+                                        handleSubmit(this.onSubmit.bind(this))
+                                    }
+                                >
+                                    <Field
+                                        props={{
+                                            label: 'Name'
+                                        }}
+                                        name='name'
+                                        component={this.renderField}
+                                    />
+                                    <br />
+                                    <Field
+                                        props={{
+                                            label: 'Email'
+                                        }}
+                                        name='email'
+                                        component={this.renderField}
+                                    />
+                                    <br />
+                                    <Field
+                                        props={{
+                                            label: 'Message',
+                                            multiLine: true,
+                                            rows: 3
+                                        }}
+                                        name='message'
+                                        component={this.renderField}
+                                    />
+                                    <br />
                                     <RaisedButton 
                                         label="Submit"
+                                        type="submit"
                                         secondary={true}
                                         fullWidth={true}
                                     />
@@ -97,4 +135,26 @@ class ContactForm extends Component {
     }
 }
 
-export default ContactForm;
+function validate(values) {
+    // console.log('values:', values);
+    const errors = {};
+
+    if (!values.name) {
+        errors.name = 'Please enter your name.';
+    }
+
+    if (!values.email) {
+        errors.email = 'Please enter a valid email address.';
+    }
+
+    if (!values.message || values.message.length < 25) {
+        errors.message = 'Please enter a message that is at least 25 characters long.';
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'MessagesNewForm'
+})(connect(null, { createMessage })(ContactForm));
